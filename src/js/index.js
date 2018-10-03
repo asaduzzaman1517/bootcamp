75955ca4e50a41ec5346c2fe53037a4cdc7dee0c
asaduzzaman1517 <asaduzzaman1517@gmail.com>
2018-10-03 10:51:06 +0600

    bootstrap 4 practice

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+
+import '../../node_modules/jquery/dist/jquery';
+import '../../node_modules/@fortawesome/fontawesome-free/js/all.min';
+import '../../node_modules/popper.js/dist/popper.min';
+import '../../node_modules/bootstrap/dist/js/bootstrap.min';
+import '../scss/style.scss';
